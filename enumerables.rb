0ebc1aebce5a42a8bc5a8240e12a0f6cc11f339f
teshager21 <teshager8922@gmail.com>
2020-05-26 13:24:04 -0600

    add my_each method for array input

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,28 @@
-puts 'hello world!'
+# rubocop:disable Style/For
+module Enumerable
+  # first the each method
+  # check the input
+  # hash, array?
+  # empty block?
+
+  # considering only arrays
+  def my_each
+    if block_given?
+      for i in self
+        yield i
+      end
+    else
+      to_enum
+     end
+   end
+end
+
+# include Enumerable
+ar = [1, 2, 'a', 3, 'b']
+
+ar.my_each do |a|
+  puts "this is: #{a}"
+end
+ar.my_each
+
+# rubocop:enable Style/For

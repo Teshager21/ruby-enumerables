19062fe6c92d8aafb0ae9daed0c55a8f6e118efc
teshager21 <teshager8922@gmail.com>
2020-05-27 05:52:53 -0600

    add #my_any

--- enumerables.rb
+++ enumerables.rb
@@ -4,7 +4,6 @@
 # rubocop:disable Metrics/CyclomaticComplexity
 # rubocop:disable Metrics/MethodLength
 module Enumerable
-  # first the each method
   # check the input
   # hash, array?
   # empty block?
@@ -13,7 +12,7 @@ module Enumerable
   def my_each
     if block_given?
       for i in 0...length
-        yield i
+        yield self[i]
       end
     else
       to_enum(:my_each)
@@ -24,7 +23,7 @@ module Enumerable
 
   def my_each_with_index
     if block_given?
-      (0...length).each do |i|
+      for i in (0...length)
         yield i, self[i]
       end
     else
@@ -64,23 +63,57 @@ module Enumerable
       end
     elsif !block_given? && arg.nil?
       my_each do |item|
-        p 'are we here?'
         return false if item.nil? or item == false
       end
     end
     true
+  end
+  #################################################
+
+  def my_any?(arg = nil)
+    if !arg.nil?
+      if arg.is_a? Regexp
+        my_each do |item|
+          return true if item.to_s.match(arg)
+        end
+      elsif arg.is_a? Class
+        my_each do |item|
+          return true if item.instance_of? arg
+        end
+      else
+        my_each do |item|
+          return true if item == arg
+        end
+      end
+    elsif block_given?
+      my_each do |item|
+        eval = yield item
+        return true if eval == true
+      end
+    elsif !block_given? && arg.nil?
+      my_each do |item|
+        return true if item.nil? == false or item != false
+      end
+    end
+    false
   end
 
   #-----------------------------------#
 end
 
 ######### End of Enumerable Module ############################
-p [].my_all?
-p [2, 4, 6, 8, 10].my_all?(Numeric)
-p [2, 4, 9].my_all?(&:even?)
-p [8, 'alen', 'smith'].my_all?(String)
-p %w[john john].my_all?('john')
+# p [].my_all?
+# p [2, 4, 6, 8, 10].my_all?(Numeric)
+# p [2, 4, 9].my_all?(&:even?)
+# p [8, 'alen', 'smith'].my_all?(String)
+# p %w[john john].my_all?('john')
 
+p { %w[ant bear cat].my_any? { |word| word.length >= 3 } }
+p { %w[ant bear cat].my_any? { |word| word.length > 4 } }
+p [nil, false, nil].my_any?(/t/)
+p [nil, true, 'h'].my_any?(Integer)
+p "#{[nil, true, 99].my_any?} :yeap"
+p [].my_any?
 # rubocop:enable Style/For
 # rubocop:enable Style/MultipleComparison
 # rubocop:enable Metrics/PerceivedComplexity
